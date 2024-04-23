09f23362b8be3f74ac87726f4320bb5f097bdd0c
Michael Durell <michael.durell2@gmail.com>
2024-04-23 18:00:34 -0500

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TBD from "../images/TBD.png";
 import Thinker from "../images/Thinker.png";
 import { Button } from "@/components/ui/button";
 // import './App.css'; // Make sure to import your CSS file
 
-function HomePage() {
+const HomePage = () => {
   const [imageVisible, setImageVisible] = useState(true);
   
   useEffect(() => {
@@ -29,10 +29,10 @@ function HomePage() {
         />
         </div>
       <div className="bottom-half">
-        <p>Transparent Budget Decisions</p>
+        <p className='font-bold text-lg'>Transparent Budget Decisions</p>
         <br></br>
         <br></br>
-        <p>"Breaking the stigma that focusing on your money is difficult."</p>
+        <p>Breaking the stigma that focusing on your money is difficult.</p>
         <div style={{ display: 'flex', justifyContent: 'center', gap: '100px', marginTop: '20px' }}>
           <Button>Demo</Button>  
           <Button>Enter</Button>  
