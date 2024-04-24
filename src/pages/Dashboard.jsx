dd93202fb83a4f8408989b0368811551ba80038b
Jose Sandoval <Software.Sandoval@gmail.com>
2024-04-23 20:31:36 -0500

    cleanup

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
-
-export default function Dashboard() {
+const Dashboard = () => {
   return (
-  <>
+    <>
+      <div>Dashboard</div>
+    </>
+  );
+};
 
-  </>
-
-  )
-}
+export default Dashboard;
