101d7230ec09c875d13298bd8889a0e42c497fc0
Michael Durell <michael.durell2@gmail.com>
2024-04-24 11:59:47 -0500

    update

--- src/pages/EnterLandingPage.jsx
+++ src/pages/EnterLandingPage.jsx
@@ -1,9 +1,22 @@
-
+// import { Link } from 'react-router-dom'; 
 
 const EnterLandingPage = () => {
+    
+    
     return (
       <>
-      <div>EnterLandingPage</div>
+      <div className="page-container">EnterLandingPage
+      <div className="top-half">
+      <p className='font-bold text-lg'>Already have an account?</p> 
+      </div>
+      <div className="bottom-half">
+      <p className='font-bold text-lg'>Don't have an account?</p> 
+      </div>
+      
+      
+      
+      
+      </div>
       </>
     )
   }
