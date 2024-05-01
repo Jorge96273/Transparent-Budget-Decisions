2de70490cc86616afdfb15cce8b13eaa7df58ed2
Jose Sandoval <Software.Sandoval@gmail.com>
2024-05-01 16:18:15 -0500

    profile working sends email verification prior to change

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateEmail, updatePassword, updateProfile, sendPasswordResetEmail } from "firebase/auth";
+import { getFirestore, collection, addDoc, query, where, getDocs } from "firebase/firestore";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
