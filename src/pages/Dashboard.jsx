0ca6e502a7a9c8f8905e1fb4379821be02c0581b
Jorge Ayala <jorge.ayalavalenzuela7@gmail.com>
2024-05-02 17:55:59 -0400

    up

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -86,9 +86,7 @@ const Dashboard = () => {
   const monthlyExpenses = accountList.filter(
     (account) => account.monthlyExpense === "Yes"
   );
-  const notMonthlyExpense = accountList.filter(
-    (account) => account.monthlyExpense === "No"
-  );
+
 
   const currentAccountBalance = (accountType) => {
     let totalDeposits = 0;
