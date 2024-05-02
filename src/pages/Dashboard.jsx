0d04eaeef13e05f9f82bafba8ef8d9afebb7c18c
Andrew_Dew <adew1009@gmail.com>
2024-05-02 10:16:52 -0500

    reverted

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/accordion";
 import BudgetedItemTable from "@/components/BudgetedItemTable";
 import { AccordionElement } from "@/components/Accordion";
-import BudgetItem from "@/components/BudgetItem";
 import LineChart from "@/components/LineChart";
 import BudgetItem from "@/components/BudgetItem";
 import CalendarChart from "@/components/CalendarChart";
